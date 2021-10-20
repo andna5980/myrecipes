4f7afc827871a63997dc4f281b175b13f677372c
andna5980 <andy5980@hotmail.com>
2021-10-20 10:41:43 +0000

    update funcionalities for flask

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -12,8 +13,11 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
 
 @app.route("/")
+app.route("/get_tasks")
 def index():
     return render_template("index.html")
 
