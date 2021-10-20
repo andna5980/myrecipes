16dd8171fa04cf46f28230fb347eac25e302ae8b
andna5980 <andy5980@hotmail.com>
2021-10-20 13:57:00 +0000

    wip

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def about():
     return render_template("about.html", page_title="About")
 
 
-@app.route("/recipes")
+@app.route("/")
 @app.route("/get_celebrities")
 def get_celebrities():
     celebrities = mongo.db.celebrities.find()
